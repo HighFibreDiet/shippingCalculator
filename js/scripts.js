6ee08cdb50e62e258e27e22c71a21d35753943f8
Robin Miller & Ben Birdsall <student@epicodus.com>
2014-02-24 13:45:33 -0800

    creates package object and methods with all specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
-var Triangle = {
-  invalid: function() {
-    return (this.badSide() || this.badSideRatio());
+var Package = {
+  shippingRate: function() {
+    return this.distanceRate() * this.speedRate();
   },
 
-  badSide: function() {
-    return(this.side1 <= 0 || this.side2 <= 0 || this.side3 <= 0);
+  speedRate: function() {
+    var speedRates = {"economy" : 1, "2-day" : 2, "overnight" : 3};
+    return speedRates[this.shippingSpeed];
   },
 
-  badSideRatio: function() {
-    return(this.side1 >= this.side2 + this.side3 || this.side2 >= this.side1 + this.side3 || this.side3 >= this.side1 + this.side2);
-  },
-
-  type: function() {
-    if(this.invalid()) {
-      return "invalid";
-    } else if(this.side1 === this.side2 && this.side2 === this.side3) {
-      return "equilateral";
-    } else if(this.side1 === this.side2 || this.side2 === this.side3 || this.side1 === this.side3) {
-      return "isosceles";
+  distanceRate: function() {
+    if (this.originCountry === this.destinationCountry) {
+      if (parseInt(this.destinationZipCode) === parseInt(this.originZipCode)) {
+        return 12;
+      } else if (Math.abs(parseInt(this.destinationZipCode) - parseInt(this.originZipCode)) < 10000) {
+        return 15;
+      } else {
+        return 17;
+      }
     } else {
-      return "scalene";
+      return 25;
     }
+  },
+
+  shippingCost: function() {
+    var rate = this.shippingRate();
+    var baseRate = 3 + this.pickUp;
+    return (rate * this.weight) + baseRate;
   }
 };
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,89 +1,304 @@
-describe("Triangle", function() {
-  describe("invalid", function() {
-    it("should return true if the three sides do not make a triangle", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 1;
-      testTriangle.side2 = 2;
-      testTriangle.side3 = 11;
-      testTriangle.invalid().should.equal(true);
+describe("Package", function() {
+  describe("shippingCost", function() {
+    it("should return the base rate for a 0lb package", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 0;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingCost().should.equal(3);
     });
 
-    it("should return false if the three sides DO make a triangle", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 4;
-      testTriangle.side2 = 5;
-      testTriangle.side3 = 7;
-      testTriangle.invalid().should.equal(false);
+    it("should return the normal economy shipping rate for a 1lb package shipped within the same domestic zip code", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 1;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingCost().should.equal(15);
     });
 
-    it("should return false if the sides are 2, 10, 11", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 2;
-      testTriangle.side2 = 10;
-      testTriangle.side3 = 11;
-      testTriangle.invalid().should.equal(false);
+    it("should return the normal economy shipping rate for a 2lb package shipped within the same domestic zip code", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingCost().should.equal(27);
+    });
+
+    it("should return the normal economy shipping rate for a 2lb package shipped domestically to a close zip code", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97217";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingCost().should.equal(33);
+    });
+
+    it("should return the normal economy shipping rate for a 2lb package shipped domestically to a non-close zip code", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "15770";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingCost().should.equal(37);
+    });
+    it("should return one more than the normal economy shipping rate for a 2lb package shipped domestically to a non-close zip code with pickup", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "15770";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = true;
+      testPackage.shippingCost().should.equal(38);
+    });
+    it("should return the overnight shipping rate for a 7lb package shipped internationally", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Canada" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "5770";
+      testPackage.weight = 7;
+      testPackage.shippingSpeed = "overnight";
+      testPackage.pickUp = false;
+      testPackage.shippingCost().should.equal(528);
     });
   });
 
-  describe("badSide", function() {
-    it("should return true for a triangle with zero or negative side length", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = -1;
-      testTriangle.side2 = 10;
-      testTriangle.side3 = 11;
-      testTriangle.badSide().should.equal(true);
+  describe("shippingRate", function() {
+    it("should equal 12 for a same-zip domestic economy shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(12);
     });
-    it("should return false for a triangle without zero or negative side length", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 100;
-      testTriangle.side2 = 10;
-      testTriangle.side3 = 11;
-      testTriangle.badSide().should.equal(false);
+    it("should equal 24 for a same-zip domestic 2-day shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "2-day";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(24);
     });
-  })
-
-
-  describe("badSideRatio", function() {
-    it("should return true a set of sides with one side too long to make a triangle with the other two", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 100;
-      testTriangle.side2 = 10;
-      testTriangle.side3 = 11;
-      testTriangle.badSideRatio().should.equal(true);
+    it("should equal 36 for a same-zip domestic overnight shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "overnight";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(36);
     });
-    it("should return false for a triangle with no side too long to make a triangle with the other two sides", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 10;
-      testTriangle.side2 = 10;
-      testTriangle.side3 = 11;
-      testTriangle.badSideRatio().should.equal(false);
+    it("should equal 15 for a close-zip domestic economy shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "90239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(15);
     });
-  })
-
-  describe("type", function() {
-    it("should return equilateral if all three sides are equal", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 4;
-      testTriangle.side2 = 4;
-      testTriangle.side3 = 4;
-      testTriangle.type().should.equal("equilateral");
+    it("should equal 30 for a close-zip domestic 2-day shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "90239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "2-day";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(30);
     });
-    it("should return isosceles if any two sides are equal", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 3;
-      testTriangle.side2 = 4;
-      testTriangle.side3 = 4;
-      testTriangle.type().should.equal("isosceles");
+    it("should equal 45 for a close-zip domestic overnight shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "90239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "overnight";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(45);
     });
-    it("should return scalene if all sides are different", function() {
-      var testTriangle = Object.create(Triangle);
-      testTriangle.side1 = 3;
-      testTriangle.side2 = 4;
-      testTriangle.side3 = 5;
-      testTriangle.type().should.equal("scalene");
+    it("should equal 17 for a far-zip domestic economy shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "05770";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(17);
+    });
+    it("should equal 34 for a far-zip domestic 2-day shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "05770";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "2-day";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(34);
+    });
+    it("should equal 51 for a far-zip domestic overnight shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "05770";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "overnight";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(51);
+    });
+    it("should equal 25 for an international economy speed shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Venezuela" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(25);
+    });
+    it("should equal 50 for an international 2-day speed shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Venezuela" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "2-day";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(50);
+    });
+    it("should equal 75 for an international overnight speed shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Venezuela" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "overnight";
+      testPackage.pickUp = false;
+      testPackage.shippingRate().should.equal(75);
     });
   });
 
+  describe("distanceRate", function() {
+    it("should equal 12 for a same-zip domestic shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.distanceRate().should.equal(12);
+    });
+    it("should equal 15 for a close-zip domestic shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "90239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.distanceRate().should.equal(15);
+    });
+    it("should equal 17 for a far-zip domestic shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "USA" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "05770";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.distanceRate().should.equal(17);
+    });
+    it("should equal 25 for an international economy speed shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Venezuela" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.distanceRate().should.equal(25);
+    });
+  });
+  describe("speedRate", function() {
+    it("should equal 1 for an economy speed shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Venezuela" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "economy";
+      testPackage.pickUp = false;
+      testPackage.speedRate().should.equal(1);
+    });
+    it("should equal 2 for a 2-day speed shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Venezuela" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "2-day";
+      testPackage.pickUp = false;
+      testPackage.speedRate().should.equal(2);
+    });
+    it("should equal 3 for an overnight speed shipment", function() {
+      var testPackage = Object.create(Package);
+      testPackage.originCountry = "USA";
+      testPackage.destinationCountry = "Venezuela" ;
+      testPackage.originZipCode = "97239";
+      testPackage.destinationZipCode = "97239";
+      testPackage.weight = 2;
+      testPackage.shippingSpeed = "overnight";
+      testPackage.pickUp = false;
+      testPackage.speedRate().should.equal(3);
+    });
+  });
 });
 
   
